1b653bba1f32ba401920639df270a3c5815da3d8
oxdjww <oxdjww@gmail.com>
2023-09-20 15:48:11 +0900

    [+] add AppConfig & seperate concerns with constructor DI

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,13 +2,27 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    /**
+     * OCP 위반 : RateDiscountPolicy를 추가했다고 해서 serviceImpl의 코드를 고치는 상황이 나와서는 안됨
+     * DIP 위반 : ServiceImpl의 코드를 보면, abstract한 interface인 DiscountPolicy뿐만 아니라 Fix/Rate DiscountPolicy에도 의존하고 있음을 확인할 수 있음.
+     * solution : 구현체가 아닌, 추상 인터페이스에만 의존하도록 해야함
+     */
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
