9b7e2d6a727432fceb70e68090042eae93bbad03
oxdjww <oxdjww@gmail.com>
2023-09-24 00:43:02 +0900

    [+] add SignletonService test

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,48 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        // 1. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+        // 2. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        // 참조값이 다름
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        assertThat(memberService1).isNotSameAs(memberService2);
+
+        /**
+         * 이를 해결할 수 있는 방안?
+         * 객체를 한 개만 생성하고 공유하도록 설계하기
+         * -> 싱글톤 패턴
+         */
+
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    public void singletonServiceTest() {
+        SingletonService instance1 = SingletonService.getInstance();
+        SingletonService instance2 = SingletonService.getInstance();
+
+        System.out.println("instance1 = " + instance1);
+        System.out.println("instance2 = " + instance2);
+        // 같은 객체
+        assertThat(instance1).isSameAs(instance2);
+    }
+}
