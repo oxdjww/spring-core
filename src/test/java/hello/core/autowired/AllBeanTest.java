c5ec444c489ac624efe67374bf0dd0bb022b29b9
oxdjww <oxdjww@gmail.com>
2023-09-26 18:29:17 +0900

    [+] add AllBeanTest (ex. discountService에서는 모든 할인 정책을 주입받아서, 선택하는 로직을 가짐)

---
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,52 @@
+package hello.core.autowired;
+
+import hello.core.AutoAppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class AllBeanTest {
+    @Test
+    void findAllBean() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        // 고정할인정책 테스트
+        int fixDiscountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        assertThat(fixDiscountPrice).isEqualTo(1000);
+        // 정률할인정책 테스트
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+    }
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            // 할인 금액 반환
+            return discountPolicy.discount(member,price);
+        }
+    }
+
+}
