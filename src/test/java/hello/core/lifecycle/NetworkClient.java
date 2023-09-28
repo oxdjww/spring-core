325aaa00e0d27bd33c5b27043074eafe536a28fc
oxdjww <oxdjww@gmail.com>
2023-09-28 23:40:45 +0900

    [+] add customized init/destroy method with @Bean option

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean , DisposableBean {
+public class NetworkClient  {
     private String url;
 
     public NetworkClient() {
@@ -30,17 +30,14 @@ public class NetworkClient implements InitializingBean , DisposableBean {
         System.out.println("close " + url);
     }
 
-    // DI가 끝나면 호출되는 함수
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
