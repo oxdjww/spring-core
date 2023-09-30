8789941cc280aeabb96eb62bd9391d95a62f0ed8
oxdjww <oxdjww@gmail.com>
2023-09-30 17:54:18 +0900

    [+] add http scope test with bean exception

---
+++ src/main/java/hello/core/web/LogdemoService.java
@@ -0,0 +1,16 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogdemoService {
+
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
